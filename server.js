e840255b69d0f8e100e0c988da5cb5d60bad5ea1
PRATIKSHAGODBOLE <126340923+PRATIKSHAGODBOLE@users.noreply.github.com>
2023-09-14 23:24:17 +0530

    cors issue

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose")
 const postRoute = require('./Routes/postsRoute')
 const userRoute = require('./Routes/usersRoute')
 //require cors 
-// const cors = require('cors')
+const cors = require('cors')
 const app = express()
 require('dotenv').config()
 
@@ -23,7 +23,7 @@ app.get('/', (req, res)=>{
 //Middleware
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-// app.use(cors)
+app.use(cors)
 
 app.use('/api/posts', postRoute)
 app.use('/api/users', userRoute)
